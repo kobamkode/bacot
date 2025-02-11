dd04d4de82bd853da2c32a1d11f5019bbb419d68
Mario Wibisono <kobamkode@gmail.com>
2025-02-11 18:03:57 +0700

    wip design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,22 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <div className='chat_window'>
+    <div className="m-4">
+      <h1>BACOT!</h1>
+      <div className="chat_window p-2 mb-5 h-screen border border-slate-600 rounded-md">
         <ul>
-          <li>test 1</li>
-          <li>test 2</li>
+          <li className="p-2 mb-2 rounded-md bg-gray-500 text-white">
+            <input type="checkbox" value="test" /> Test 1
+          </li>
+          <li className="p-2 mb-2 rounded-md bg-gray-500 text-white">
+            <input type="checkbox" value="test" /> Test 2
+          </li>
         </ul>
       </div>
-      <div className='chat_message'>
-        <form action={addMessage}>
-          <input className='rounded-full' name='message' placeholder='Message' />
-          <button type='submit'>Submit</button>
+      <div className="chat_message">
+        <form action={addMessage} className="flex">
+          <input className="rounded-md flex-grow" name="message" placeholder="Message" />
+          <button className="p-2.5 ml-2 border border-slate-600 rounded-md bg-slate-800 text-white" type="submit">Submit</button>
         </form>
       </div>
     </div>
