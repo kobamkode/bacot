2547ce9df118db6185b42a06fd20bc207117d15e
Mario Wibisono <kobamkode@gmail.com>
2025-02-12 17:58:17 +0700

    add task

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+'use client'
+
+import { KeyboardEvent, useState } from "react";
+
 export default function Home() {
-  async function addMessage(formData: FormData) {
-    'use server'
-    console.log(formData.get('message'))
+  const [message, setMessage] = useState<string>("")
+  const [messages, setMessages] = useState<string[]>([])
+
+  const addMessage = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && message.trim() !== '') {
+      e.preventDefault()
+      setMessages(prevMessages => [...prevMessages, message])
+      setMessage('')
+    }
   }
 
   return (
@@ -9,18 +19,22 @@ export default function Home() {
       <h1>BACOT!</h1>
       <div className="chat_window p-2 mb-5 h-screen border border-slate-600 rounded-md">
         <ul>
-          <li className="p-2 mb-2 rounded-md bg-gray-500 text-white">
-            <input type="checkbox" value="test" /> Test 1
-          </li>
-          <li className="p-2 mb-2 rounded-md bg-gray-500 text-white">
-            <input type="checkbox" value="test" /> Test 2
-          </li>
+          {messages.map((item, index) => (
+            <li key={index} className="p-2 mb-2 rounded-md bg-gray-500 text-white">
+              <input type="checkbox" /> {item}
+            </li>
+          ))}
         </ul>
       </div>
       <div className="chat_message">
-        <form action={addMessage} className="flex">
-          <input className="rounded-md flex-grow" name="message" placeholder="Message" />
-          <button className="p-2.5 ml-2 border border-slate-600 rounded-md bg-slate-800 text-white" type="submit">Submit</button>
+        <form className="flex">
+          <input id="message" className="rounded-md flex-grow"
+            name="message"
+            placeholder="Message"
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+            onKeyDown={addMessage}
+          />
         </form>
       </div>
     </div>
